fc6bc81bb0b75d0d1d30bc639632fa70630f325b
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-11-03 21:12:12 +0530

    Updated

--- app/travel/components/Header.jsx
+++ app/travel/components/Header.jsx
@@ -1,12 +1,8 @@
-import {
-  BellAlertIcon,
-  HeartIcon,
-  PlusIcon,
-} from "@heroicons/react/24/outline";
+import { CursorArrowRaysIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Interaction", icon: HeartIcon, href: "/" },
-  { name: "Notification", icon: BellAlertIcon, href: "/" },
+  { name: "Book Rides", href: "/" },
+  { name: "Book Tours", href: "/" },
 ];
 
 export default function Header() {
@@ -14,35 +10,24 @@ export default function Header() {
     <>
       <header className="sticky top-0 h-[70px] w-screen backdrop-blur-3xl ps-20 pe-6 border-b border-gray-300 flex items-center">
         <nav className="grid grid-cols-12 items-center justify-between px-4 w-full">
-          <div className="col-span-6 rounded-xl flex items-center justify-start gap-3 w-full">
+          <div className="col-span-4 rounded-xl flex items-center justify-start gap-3 w-full">
             <h1 className="text-2xl font-semibold text-lime-800 tracking-tighter">
               Whizzed
             </h1>
           </div>
-          <div className="col-span-6 flex items-center justify-end space-x-4">
-            <div className="flex items-center gap-4">
-              <div className="group relative px-3 h-9 mx-auto bg-lime-50 hover:bg-lime-100 ring-1 ring-lime-200 hover:ring-lime-500 shadow-inner transition duration-300 ease-in-out rounded-xl flex items-center justify-center gap-2">
-                <h1 className="text-sm font-medium text-lime-800 tracking-tight">
-                  Create
+          <div className="col-span-4 flex items-center justify-center gap-16">
+            {navigation.map((item, index) => (
+              <div key={index} className="">
+                <h1 className="text-lime-800 font-semibold tracking-tight">
+                  {item.name}
                 </h1>
-                <PlusIcon className="w-5 h-5 stroke-lime-800" />
               </div>
-              {navigation.map((item, index) => (
-                <div
-                  key={index}
-                  className="group relative w-9 h-9 mx-auto bg-lime-50 hover:bg-lime-100 ring-1 ring-lime-200 hover:ring-lime-500 shadow-inner transition duration-300 ease-in-out rounded-xl flex items-center justify-center"
-                >
-                  <item.icon className="w-5 h-5 stroke-lime-800" />
-                </div>
-              ))}
-            </div>
-            <div className="group rounded-xl ring-1 ring-lime-200 hover:ring-lime-500 transition duration-300 ease-in-out shadow-inner bg-lime-50 hover:bg-lime-100">
-              <input
-                type="search"
-                className="bg-transparent px-3 w-96 h-9 text-sm rounded-full border-0 focus:ring-0 placeholder:text-lime-800 placeholder:text-sm placeholder:font-medium placeholder:tracking-tight group-hover:placeholder:text-lime-800"
-                placeholder="Search anything.."
-              />
-            </div>
+            ))}
+          </div>
+          <div className="col-span-4 ms-auto">
+            <button className="flex items-center gap-2 text-lime-800 text-sm font-semibold bg-lime-50 hover:bg-lime-100 transition duration-300 ease-in-out px-4 py-2 rounded-xl ring-1 ring-lime-800 shadow-inner">
+              <CursorArrowRaysIcon className="w-6 h-6" /> Create Ride/Tour
+            </button>
           </div>
         </nav>
       </header>
--- app/travel/page.js
+++ app/travel/page.js
@@ -1,13 +1,3 @@
-import Header from "./components/Header";
-
 export default function Travel(params) {
-  return (
-    <>
-      {/* <Header /> */}
-
-      <div className="text-9xl ms-[70px] isolate">
-        <h1>Header</h1>
-      </div>
-    </>
-  );
+  return <></>;
 }
